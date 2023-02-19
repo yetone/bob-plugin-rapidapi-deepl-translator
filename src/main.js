4549ad5b61cf82a8b2a0f8c80ebd33181af8e089
yetone <yetoneful@gmail.com>
2023-02-19 16:17:42 +0800

    feat: add desc for api key input

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-/**
- * 由于各大服务商的语言代码都不大一样，
- * 所以我定义了一份 Bob 专用的语言代码，以便 Bob 主程序和插件之间互传语种。
- * Bob 语言代码列表 https://ripperhe.gitee.io/bob/#/plugin/addtion/language
- *
- * 转换的代码建议以下面的方式实现，
- * `xxx` 代表服务商特有的语言代码，请替换为真实的，
- * 具体支持的语种数量请根据实际情况而定。
- *
- * Bob 语言代码转服务商语言代码(以为 'zh-Hans' 为例): var lang = langMap.get('zh-Hans');
- * 服务商语言代码转 Bob 语言代码: var standardLang = langMapReverse.get('xxx');
- */
-
 var items = [
     ["auto", "auto"],
     ["zh-Hans", "zh"],
@@ -28,7 +15,7 @@ function supportLanguages() {
 }
 
 function translate(query, completion) {
-    const api_keys = $option.api_key.split(",").map((key) => key.trim());
+    const api_keys = $option.api_keys.split(",").map((key) => key.trim());
     const api_key = api_keys[Math.floor(Math.random() * api_keys.length)];
     const header = {
         "Content-Type": "application/json",
